b47ceba55a234d147df9739bcc72454d4279edb9
Asiel Jimenez <asieluniversestinson@gmail.com>
2025-06-20 12:38:13 -0600

    formulario cargado

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,1 +1,21 @@
-/* hola soy yo */
+document.addEventListener("DOMContentLoaded", () =>{
+
+    fetch("https://digitalapiqa.fuller.com.mx/api/Auth/CheckUser")
+
+    .then(objeto, objeto.json())
+    .then(datos => {
+        const contenedor = document.getElementById("id-datos");
+
+        datos..forEach(funcionFlecha =>{
+            const tarjeta = document.createElement('div');
+            tarjeta.classList.add("tarjeta");
+
+            const username = document.createElement('p');
+            username.textContent = funcionFlecha.userName;
+            username.classList.add("texto-tarjeta");
+
+            tarjeta.appendChild(username);
+            c
+        });
+    });
+});
